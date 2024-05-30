d2a8b9c403c32f7c97fd44e8d7d2f7dcb57996dc
rizal fauzan <rizalfauzan190@gmail.com>
2024-05-30 07:29:56 +0700

    (feat: done darkmode )

--- index.js
+++ index.js
@@ -4,6 +4,50 @@ $(document).ready(function() {
     var dots = $('.footerdot');
     var slideInterval;
 
+    function toggleDarkMode() {
+        $('body').toggleClass('dark-mode');
+        $('header').toggleClass('dark-mode');
+        $('nav ul li').toggleClass('dark-mode');
+        $('a').toggleClass('dark-mode')
+        $('.hero-section').toggleClass('dark-mode');
+        $('.service').toggleClass('dark-mode');
+        $('.about-us').toggleClass('dark-mode');
+        $('.footer-content').toggleClass('dark-mode');
+        $('.card').toggleClass('dark-mode');
+        $('.cards').toggleClass('dark-mode');
+        $('.logo').toggleClass('dark-mode')
+        $('.dot-section .footerdot').toggleClass('dark-mode');
+
+        var newLogoSrc = $('body').hasClass('dark-mode') ? './assets/logo2.png' : './assets/logo.png';
+        var newBgSrc = $('body').hasClass('dark-mode') ? './assets/background2.png' : './assets/background.png';
+        $('#logo-image').attr('src', newLogoSrc);
+        $('#logo-services').attr('src', newLogoSrc);
+        $('#logo-card1').attr('src', newLogoSrc);
+        $('#logo-card2').attr('src', newLogoSrc);
+        $('#logo-card3').attr('src', newLogoSrc);
+        $('#logo-about').attr('src', newLogoSrc);
+        $('#logo-footer').attr('src', newLogoSrc);
+        $('#bg').css('background-image', 'url(' + newBgSrc + ')');
+
+        if ($('body').hasClass('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+
+        if ($('body').hasClass('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    }
+
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        toggleDarkMode();
+    }
+
+    $('#dark-mode-toggle').click(toggleDarkMode);
+
     function showSlide(index) {
         var offset = -index * 100 + '%';
         $('.slider-wrapper').css('transform', 'translateX(' + offset + ')');
@@ -41,16 +85,4 @@ $(document).ready(function() {
 
     showSlide(currentIndex);
     startSlider();
-});
-
-
-const darKMode = document.querySelector('.logo');
-const bodyColor = document.querySelector('body');
-const headerColor = document.querySelector('header');
-const pColor = document.querySelector('.p-content, .hero-content');
-
-darKMode.addEventListener('click', () => {
-    bodyColor.style.backgroundColor = 'black';
-    headerColor.style.backgroundColor = 'black';
-    pColor.style.color = "white"
 });
